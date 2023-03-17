5ec73777d1ebf2457ac85a8a43152745a0c4c906
stefanymelda <juwitastefany13@gmail.com>
2023-03-17 15:16:39 +0700

    push stefanyapp ok

--- controller/controller.go
+++ controller/controller.go
@@ -1,7 +1,7 @@
 package controller
 
 import (
-	"iteung/config"
+	"github.com/stefanymelda/stefanyapp/config"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
@@ -19,7 +19,7 @@ func PostWhatsAuthRequest(c *fiber.Ctx) error {
 		if err != nil {
 			return err
 		}
-		ntfbtn := whatsauth.RunModule(req, config.PrivateKey, config.Usertables[:], config.Ulbimariaconn)
+		ntfbtn := whatsauth.RunModuleLegacy(req, config.PrivateKey, config.Usertables[:], config.Ulbimariaconn)
 		return c.JSON(ntfbtn)
 	} else {
 		var ws whatsauth.WhatsauthStatus
--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"log"
 
-	"iteung/config"
+	"github.com/stefanymelda/stefanyapp/config"
 
 	"github.com/aiteung/musik"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	"github.com/whatsauth/whatsauth"
 
-	"iteung/url"
+	"github.com/stefanymelda/stefanyapp/url"
 
 	"github.com/gofiber/fiber/v2"
 )
