7b9bc39446cff26f4439ecb056523a7f99566014
stefanymelda <juwitastefany13@gmail.com>
2023-05-31 15:45:59 +0700

    latihanweek11

--- controller/coba.go
+++ controller/coba.go
@@ -253,6 +253,18 @@ func InsertData(c *fiber.Ctx) error {
 	})
 }
 
+// UpdateData godoc
+// @Summary Update data presensi.
+// @Description Ubah data presensi.
+// @Tags Presensi
+// @Accept json
+// @Produce json
+// @Param id path string true "Masukan ID"
+// @Param request body Presensi true "Payload Body [RAW]"
+// @Success 200 {object} Presensi
+// @Failure 400
+// @Failure 500
+// @Router /upd/{id} [put]
 func UpdateData(c *fiber.Ctx) error {
 	db := config.Ulbimongoconn
 
