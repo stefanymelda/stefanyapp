6b3855c56e5bbf295bdce6d0c832d3d9efb896df
stefanymelda <juwitastefany13@gmail.com>
2023-05-31 14:37:57 +0700

    latihanweek11

--- docs/docs.go
+++ docs/docs.go
@@ -10,7 +10,11 @@ const docTemplate = `{
     "info": {
         "description": "{{escape .Description}}",
         "title": "{{.Title}}",
-        "contact": {},
+        "contact": {
+            "name": "API Support",
+            "url": "https://github.com/stefanymelda",
+            "email": "1214026@std.ulbi.ac.id"
+        },
         "version": "{{.Version}}"
     },
     "host": "{{.Host}}",
@@ -20,12 +24,12 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "",
-	Host:             "",
-	BasePath:         "",
-	Schemes:          []string{},
-	Title:            "",
-	Description:      "",
+	Version:          "1.0",
+	Host:             "jaehyun.herokuapp.com",
+	BasePath:         "/",
+	Schemes:          []string{"https", "http"},
+	Title:            "TES SWAG",
+	Description:      "This is a sample server.",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
