a32e2c564bd2d197bd232321ea78de43f6b02414
stefanymelda <juwitastefany13@gmail.com>
2023-06-16 14:20:11 +0700

    cobaTB

--- config/cors.go
+++ config/cors.go
@@ -17,6 +17,7 @@ var origins = []string{
 	"https://iteung.ulbi.ac.id",
 	"https://whatsauth.github.io",
 	"https://stefanymelda.github.io",
+	"http://127.0.0.1:5500",
 }
 
 var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
