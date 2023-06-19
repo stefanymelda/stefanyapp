0a69e7594378dadf7c3c35cacc1388359902b34f
stefanymelda <juwitastefany13@gmail.com>
2023-06-19 15:47:37 +0700

    ehe

--- controller/coba.go
+++ controller/coba.go
@@ -160,209 +160,6 @@ func InsertDataSurvey(c *fiber.Ctx) error {
 	})
 }
 
-// GetAllPresensi godoc
-// @Summary Get All Data Presensi.
-// @Description Mengambil semua data presensi.
-// @Tags Presensi
-// @Accept json
-// @Produce json
-// @Success 200 {object} Presensi
-// @Router /presensi [get]
-// func GetAllPresensi(c *fiber.Ctx) error {
-// 	ps := inimodullatihan.GetAllPresensi(config.Ulbimongoconn, "presensi")
-// 	return c.JSON(ps)
-// }
-
-// GetPresensiID godoc
-// @Summary Get By ID Data Presensi.
-// @Description Ambil per ID data presensi.
-// @Tags Presensi
-// @Accept json
-// @Produce json
-// @Param id path string true "Masukan ID"
-// @Success 200 {object} Presensi
-// @Failure 400
-// @Failure 404
-// @Failure 500
-// @Router /presensi/{id} [get]
-// func GetPresensiID(c *fiber.Ctx) error {
-// 	id := c.Params("id")
-// 	if id == "" {
-// 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-// 			"status":  http.StatusInternalServerError,
-// 			"message": "Wrong parameter",
-// 		})
-// 	}
-// 	objID, err := primitive.ObjectIDFromHex(id)
-// 	if err != nil {
-// 		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
-// 			"status":  http.StatusBadRequest,
-// 			"message": "Invalid id parameter",
-// 		})
-// 	}
-// 	ps, err := inimodullatihan.GetPresensiFromID(objID, config.Ulbimongoconn, "presensi")
-// 	if err != nil {
-// 		if errors.Is(err, mongo.ErrNoDocuments) {
-// 			return c.Status(http.StatusNotFound).JSON(fiber.Map{
-// 				"status":  http.StatusNotFound,
-// 				"message": fmt.Sprintf("No data found for id %s", id),
-// 			})
-// 		}
-// 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-// 			"status":  http.StatusInternalServerError,
-// 			"message": fmt.Sprintf("Error retrieving data for id %s", id),
-// 		})
-// 	}
-// 	return c.JSON(ps)
-// }
-
-// func GetAllKuesioner(c *fiber.Ctx) error {
-// 	ps := inimodule.GetAllKuesioner(config.Ulbimongoconn, "kuesioner")
-// 	return c.JSON(ps)
-// }
-
-// InsertData godoc
-// @Summary Insert data presensi.
-// @Description Input data presensi.
-// @Tags Presensi
-// @Accept json
-// @Produce json
-// @Param request body Presensi true "Payload Body [RAW]"
-// @Success 200 {object} Presensi
-// @Failure 400
-// @Failure 500
-// @Router /ins [post]
-// func InsertData(c *fiber.Ctx) error {
-// 	db := config.Ulbimongoconn
-// 	var presensi tuhmodelloh.Presensi
-// 	if err := c.BodyParser(&presensi); err != nil {
-// 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-// 			"status":  http.StatusInternalServerError,
-// 			"message": err.Error(),
-// 		})
-// 	}
-// 	insertedID, err := tuhmoduleloh.InsertPresensi(db, "presensi",
-// 		presensi.Longitude,
-// 		presensi.Latitude,
-// 		presensi.Location,
-// 		presensi.Phone_number,
-// 		presensi.Checkin,
-// 		presensi.Biodata)
-// 	if err != nil {
-// 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-// 			"status":  http.StatusInternalServerError,
-// 			"message": err.Error(),
-// 		})
-// 	}
-// 	return c.Status(http.StatusOK).JSON(fiber.Map{
-// 		"status":      http.StatusOK,
-// 		"message":     "Data berhasil disimpan.",
-// 		"inserted_id": insertedID,
-// 	})
-// }
-
-// UpdateData godoc
-// @Summary Update data presensi.
-// @Description Ubah data presensi.
-// @Tags Presensi
-// @Accept json
-// @Produce json
-// @Param id path string true "Masukan ID"
-// @Param request body Presensi true "Payload Body [RAW]"
-// @Success 200 {object} Presensi
-// @Failure 400
-// @Failure 500
-// @Router /upd/{id} [put]
-// func UpdateData(c *fiber.Ctx) error {
-// 	db := config.Ulbimongoconn
-
-// 	// Get the ID from the URL parameter
-// 	id := c.Params("id")
-
-// 	// Parse the ID into an ObjectID
-// 	objectID, err := primitive.ObjectIDFromHex(id)
-// 	if err != nil {
-// 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-// 			"status":  http.StatusInternalServerError,
-// 			"message": err.Error(),
-// 		})
-// 	}
-
-// 	// Parse the request body into a Presensi object
-// 	var presensi tuhmodelloh.Presensi
-// 	if err := c.BodyParser(&presensi); err != nil {
-// 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-// 			"status":  http.StatusInternalServerError,
-// 			"message": err.Error(),
-// 		})
-// 	}
-
-// 	// Call the UpdatePresensi function with the parsed ID and the Presensi object
-// 	err = tuhmoduleloh.UpdatePresensi(db, "presensi",
-// 		objectID,
-// 		presensi.Longitude,
-// 		presensi.Latitude,
-// 		presensi.Location,
-// 		presensi.Phone_number,
-// 		presensi.Checkin,
-// 		presensi.Biodata)
-// 	if err != nil {
-// 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-// 			"status":  http.StatusInternalServerError,
-// 			"message": err.Error(),
-// 		})
-// 	}
-
-// 	return c.Status(http.StatusOK).JSON(fiber.Map{
-// 		"status":  http.StatusOK,
-// 		"message": "Data successfully updated",
-// 	})
-// }
-
-// DeletePresensiByID godoc
-// @Summary Delete data presensi.
-// @Description Hapus data presensi.
-// @Tags Presensi
-// @Accept json
-// @Produce json
-// @Param id path string true "Masukan ID"
-// @Success 200
-// @Failure 400
-// @Failure 500
-// @Router /delete/{id} [delete]
-// func DeletePresensiByID(c *fiber.Ctx) error {
-// 	id := c.Params("id")
-// 	if id == "" {
-// 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-// 			"status":  http.StatusInternalServerError,
-// 			"message": "Wrong parameter",
-// 		})
-// 	}
-
-// 	objID, err := primitive.ObjectIDFromHex(id)
-// 	if err != nil {
-// 		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
-// 			"status":  http.StatusBadRequest,
-// 			"message": "Invalid id parameter",
-// 		})
-// 	}
-
-// 	err = tuhmoduleloh.DeletePresensiByID(objID, config.Ulbimongoconn, "presensi")
-// 	if err != nil {
-// 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-// 			"status":  http.StatusInternalServerError,
-// 			"message": fmt.Sprintf("Error deleting data for id %s", id),
-// 		})
-// 	}
-
-// 	return c.Status(http.StatusOK).JSON(fiber.Map{
-// 		"status":  http.StatusOK,
-// 		"message": fmt.Sprintf("Data with id %s deleted successfully", id),
-// 	})
-// }
-
-
-
 //TUGASBESAR
 
 // GetAllKuesioner godoc
@@ -557,6 +354,212 @@ func DeleteKuesionerByID(c *fiber.Ctx) error {
 		"message": fmt.Sprintf("Data with id %s deleted successfully", id),
 	})
 }
+
+//ENDTugasBesar
+
+// GetAllPresensi godoc
+// @Summary Get All Data Presensi.
+// @Description Mengambil semua data presensi.
+// @Tags Presensi
+// @Accept json
+// @Produce json
+// @Success 200 {object} Presensi
+// @Router /presensi [get]
+func GetAllPresensi(c *fiber.Ctx) error {
+	ps := inimodullatihan.GetAllPresensi(config.Ulbimongoconn, "presensi")
+	return c.JSON(ps)
+}
+
+// GetPresensiID godoc
+// @Summary Get By ID Data Presensi.
+// @Description Ambil per ID data presensi.
+// @Tags Presensi
+// @Accept json
+// @Produce json
+// @Param id path string true "Masukan ID"
+// @Success 200 {object} Presensi
+// @Failure 400
+// @Failure 404
+// @Failure 500
+// @Router /presensi/{id} [get]
+func GetPresensiID(c *fiber.Ctx) error {
+	id := c.Params("id")
+	if id == "" {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": "Wrong parameter",
+		})
+	}
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"status":  http.StatusBadRequest,
+			"message": "Invalid id parameter",
+		})
+	}
+	ps, err := inimodullatihan.GetPresensiFromID(objID, config.Ulbimongoconn, "presensi")
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return c.Status(http.StatusNotFound).JSON(fiber.Map{
+				"status":  http.StatusNotFound,
+				"message": fmt.Sprintf("No data found for id %s", id),
+			})
+		}
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": fmt.Sprintf("Error retrieving data for id %s", id),
+		})
+	}
+	return c.JSON(ps)
+}
+
+func GetAllKuesioner(c *fiber.Ctx) error {
+	ps := inimodule.GetAllKuesioner(config.Ulbimongoconn, "kuesioner")
+	return c.JSON(ps)
+}
+
+// InsertData godoc
+// @Summary Insert data presensi.
+// @Description Input data presensi.
+// @Tags Presensi
+// @Accept json
+// @Produce json
+// @Param request body Presensi true "Payload Body [RAW]"
+// @Success 200 {object} Presensi
+// @Failure 400
+// @Failure 500
+// @Router /ins [post]
+func InsertData(c *fiber.Ctx) error {
+	db := config.Ulbimongoconn
+	var presensi tuhmodelloh.Presensi
+	if err := c.BodyParser(&presensi); err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": err.Error(),
+		})
+	}
+	insertedID, err := tuhmoduleloh.InsertPresensi(db, "presensi",
+		presensi.Longitude,
+		presensi.Latitude,
+		presensi.Location,
+		presensi.Phone_number,
+		presensi.Checkin,
+		presensi.Biodata)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": err.Error(),
+		})
+	}
+	return c.Status(http.StatusOK).JSON(fiber.Map{
+		"status":      http.StatusOK,
+		"message":     "Data berhasil disimpan.",
+		"inserted_id": insertedID,
+	})
+}
+
+// UpdateData godoc
+// @Summary Update data presensi.
+// @Description Ubah data presensi.
+// @Tags Presensi
+// @Accept json
+// @Produce json
+// @Param id path string true "Masukan ID"
+// @Param request body Presensi true "Payload Body [RAW]"
+// @Success 200 {object} Presensi
+// @Failure 400
+// @Failure 500
+// @Router /upd/{id} [put]
+func UpdateData(c *fiber.Ctx) error {
+	db := config.Ulbimongoconn
+
+	// Get the ID from the URL parameter
+	id := c.Params("id")
+
+	// Parse the ID into an ObjectID
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": err.Error(),
+		})
+	}
+
+	// Parse the request body into a Presensi object
+	var presensi tuhmodelloh.Presensi
+	if err := c.BodyParser(&presensi); err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": err.Error(),
+		})
+	}
+
+	// Call the UpdatePresensi function with the parsed ID and the Presensi object
+	err = tuhmoduleloh.UpdatePresensi(db, "presensi",
+		objectID,
+		presensi.Longitude,
+		presensi.Latitude,
+		presensi.Location,
+		presensi.Phone_number,
+		presensi.Checkin,
+		presensi.Biodata)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": err.Error(),
+		})
+	}
+
+	return c.Status(http.StatusOK).JSON(fiber.Map{
+		"status":  http.StatusOK,
+		"message": "Data successfully updated",
+	})
+}
+
+// DeletePresensiByID godoc
+// @Summary Delete data presensi.
+// @Description Hapus data presensi.
+// @Tags Presensi
+// @Accept json
+// @Produce json
+// @Param id path string true "Masukan ID"
+// @Success 200
+// @Failure 400
+// @Failure 500
+// @Router /delete/{id} [delete]
+func DeletePresensiByID(c *fiber.Ctx) error {
+	id := c.Params("id")
+	if id == "" {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": "Wrong parameter",
+		})
+	}
+
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"status":  http.StatusBadRequest,
+			"message": "Invalid id parameter",
+		})
+	}
+
+	err = tuhmoduleloh.DeletePresensiByID(objID, config.Ulbimongoconn, "presensi")
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": fmt.Sprintf("Error deleting data for id %s", id),
+		})
+	}
+
+	return c.Status(http.StatusOK).JSON(fiber.Map{
+		"status":  http.StatusOK,
+		"message": fmt.Sprintf("Data with id %s deleted successfully", id),
+	})
+}
+
+
+
 
 
 
